f1d0494749975ca30609fb82b6d2902d049789e7
JOCARV <joao.carvalho@mercedes-benz.io>
2023-11-27 12:04:08 +0000

    And Environments

--- src/test/java/hook/Hook.java
+++ src/test/java/hook/Hook.java
@@ -4,9 +4,16 @@ import com.saucelabs.saucebindings.SauceSession;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import lombok.SneakyThrows;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import step_definition.TestContext;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Properties;
 
 public class Hook {
 
@@ -20,8 +27,10 @@ public class Hook {
     public void setUp() {
     }
 
+    @SneakyThrows
     @After
     public void tearDown(Scenario scenario) {
+        setAllureEnvironment();
         var driverManager = testContext.getWebDriverManager();
         SauceSession session = driverManager.getSession();
         if (session != null) {
@@ -29,8 +38,26 @@ public class Hook {
         } else {
             String result = scenario.isFailed() ? "failed" : "passed";
             JavascriptExecutor js = (JavascriptExecutor) driverManager.getDriver();
-            js.executeScript(String.format("sauce:job-result=%s",result));
+            js.executeScript(String.format("sauce:job-result=%s", result));
             driverManager.getDriver().quit();
         }
+    }
+
+    private void setAllureEnvironment() throws IOException {
+        Properties properties = new Properties();
+        Capabilities caps = ((RemoteWebDriver) testContext.getWebDriverManager().getDriver()).getCapabilities();
+        var browserName = caps.getBrowserName();
+        var browserVersion = caps.getBrowserVersion();
+        properties.computeIfAbsent("Browser(version)", k -> String.format("%s(%s)", browserName, browserVersion));
+        var platform = caps.getPlatformName();
+        properties.computeIfAbsent("Platform", k -> platform.name());
+        String propertiesFilePath = "target/allure-results/environment.properties";
+        File file = new File(propertiesFilePath);
+        if (file.getParentFile().mkdirs()) {
+            if (file.createNewFile()) {
+                properties.store(new FileWriter(file), "Store environment");
+            }
+        }
+
     }
 }
